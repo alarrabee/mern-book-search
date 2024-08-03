e1950aa4414b8995c258ab3ac4bcc50534bf9351
alarrabee <alexlarrabee10@gmail.com>
2024-08-03 15:13:34 -0500

    add required dependencies to support the Apollo server. refactor server.js to connect to Apollo server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,49 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { typeDefs, resolvers } = require("./schemas");
+const { json } = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+const startApolloServer = async () => {
+	await server.start();
+		app.use(
+			"/graphql",
+			json(),
+			expressMiddleware(server, {
+				context: async ({ req }) => ({ token: req.headers.token }),
+			}),
+		);
+	
+	app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}`);
+	});
+};
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
+
+startApolloServer(typeDefs, resolvers);
