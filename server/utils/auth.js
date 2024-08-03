6fac3afea87777e67f70e4f0064b6410962b5b2f
alarrabee <alexlarrabee10@gmail.com>
2024-08-03 15:45:59 -0500

    refactor auth.js to work with graphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,23 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
+
 module.exports = {
+    AuthenticationError: new GraphQLError('Could not authenticate user', {
+			extensions: {
+				code: 'UNAUTHENTICATED',
+			},
+		}),
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+	//authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // let token = req.query.token || req.headers.authorization;
+		let token = req.query.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -29,7 +38,8 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    // next();
+		return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
