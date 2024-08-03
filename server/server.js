3948b512a9645fd1e3fc28a222dfdf71ccc40edc
alarrabee <alexlarrabee10@gmail.com>
2024-08-03 15:36:18 -0500

    refactor server.js to connect to Apollo server. add middleware for authentication

--- server/server.js
+++ server/server.js
@@ -1,49 +1,56 @@
 const express = require('express');
-// const path = require('path');
-// const db = require('./config/connection');
-// const routes = require('./routes');
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
+
 const { typeDefs, resolvers } = require("./schemas");
-const { json } = require("body-parser");
+const db = require('./config/connection');
+// const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
 
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
 	await server.start();
-		app.use(
-			"/graphql",
-			json(),
-			expressMiddleware(server, {
-				context: async ({ req }) => ({ token: req.headers.token }),
-			}),
-		);
+
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
 	
-	app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}`);
-	});
+	app.use("/graphql", expressMiddleware(server, {
+				context: authMiddleware 
+	}));
+
+	// if we're in production, serve client/build as static assets
+	if (process.env.NODE_ENV === 'production') {
+  	app.use(express.static(path.join(__dirname, '../client/dist')));
+
+		app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+	}
+	
+	db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
 };
 
-// if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+
 
 // app.use(routes);
 
 // db.once('open', () => {
 //   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 // });
+
+// Call the async function to start the server
 
 startApolloServer(typeDefs, resolvers);
